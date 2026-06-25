654177d99db31cd63b00a08bb4943d1f47494c22
Achu0987 <harshithacse973@gmail.com>
2026-06-25 10:15:45 +0530

    Update vite config for Github pages base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/harshitha-portfolio/',
   plugins: [react(), viteCompression(), cssInjectedByJsPlugin()],
   build: {
     target: 'esnext',
